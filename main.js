861fb0dafaadf7f09a599195cdb07449ba37f36e
azrizhaziq <azrizhaziq@gmail.com>
2021-06-07 19:23:24 +0800

    Init slide with vite and reveal js

---
+++ main.js
@@ -0,0 +1,19 @@
+import "reveal.js/dist/reveal.css";
+import "reveal.js/dist/theme/blood.css";
+import "reveal.js/plugin/highlight/zenburn.css";
+
+import Reveal from "reveal.js";
+import Notes from "reveal.js/plugin/notes/notes.esm";
+import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
+import HighLight from "reveal.js/plugin/highlight/highlight.esm";
+
+let deck = new Reveal({
+  controls: true,
+  progress: true,
+  history: true,
+  center: true,
+  slideNumber: true,
+  plugins: [Markdown, HighLight, Notes],
+});
+
+deck.initialize();
